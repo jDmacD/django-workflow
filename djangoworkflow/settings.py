217d60fa07678d9f9d31991117b0b2476d836f86
jDmacD <jmacdoo@gmail.com>
2025-04-26 14:28:06 +0100

    update allowed hosts

--- djangoworkflow/settings.py
+++ djangoworkflow/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m&3y7^ef&%!prcqyovkhj-hn72@0yt6p-v!g9@v(=p=^p=b+mx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
